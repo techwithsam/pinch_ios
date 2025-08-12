d3dac2d8b07ac5e2ea727de1d428eec90a55eb09
Samuel Adekunle <samuelbeebest@gmail.com>
2025-08-12 21:18:47 +0100

    Drag Gesture Added

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -28,6 +36,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -36,12 +45,24 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
-                    .animation(.linear(duration: 1), value: isAnimating)
+//                    .animation(.linear(duration: 1), value: isAnimating)
+                // MARK: - DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)

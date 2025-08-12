5322446f775a564751b76bd56fce36b7773b7b90
Samuel Adekunle <samuelbeebest@gmail.com>
2025-08-12 20:47:24 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Samuel Adekunle on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
